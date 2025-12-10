8b578aa82b70c580b350b632d6ab183c1d91f1e4
admin <admin@DESKTOP-I6HUGBR>
2025-12-10 10:56:47 +0900

    Homework 6

---
+++ Homework/src/B.java
@@ -0,0 +1,6 @@
+
+public class B {
+	public static void main(String[] args) {
+		System.out.println("Bob branch homework 6");
+	}
+}
--- Homework/src/Homework.java
+++
@@ -1,6 +0,0 @@
-
-public class Homework {
-	public static void main(String[] args) {
-		System.out.println("This is Homework");
-	}
-}
