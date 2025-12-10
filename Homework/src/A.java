f3724b6d9a2fa035dce46602400d51f579512df6
admin <admin@DESKTOP-I6HUGBR>
2025-12-10 10:51:04 +0900

    Homework (3) Level

--- Homework/src/A.java
+++ Homework/src/A.java
@@ -2,6 +2,6 @@
 public class A {
 	public static void main(String[] args) {
 		System.out.println("This is Homework");
-		System.out.println("This is Homework (3) Level");
+		System.out.println("This is Homework (3)  Level");
 	}
 }
--- Homework/src/C.java
+++ Homework/src/C.java
@@ -1,6 +1,6 @@
 
 public class C {
 	public static void main(String[] args) {
-		System.out.println("This is Homework (3) Level");
+		System.out.println("This is Homework (3)  Level");
 	}
 }
